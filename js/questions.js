05ad5559ba414b528a35996db19097e107ac688d
suheri77 <153284084+suheri77@users.noreply.github.com>
2024-05-03 16:20:25 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,200 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Apa dampak sosial utama dari penemuan komputer pada abad ke-20?",
+    answer: "Transformasi dalam pendidikan dan pekerjaan",
+    options: [
+      "Perubahan pola ekonomi",
+      "Perkembangan industri otomotif",
+      "Transformasi dalam pendidikan dan pekerjaan",
+      "Penurunan minat dalam seni dan budaya",
+      "Peningkatan penggunaan obat-obatan terlarang"
+      
+    ]
+  },
+    {
+    numb: 2,
+    question: "Peristiwa apa yang menandai awal era komputer pribadi dan dampaknya terhadap masyarakat?",
+    answer: "Peluncuran komputer Apple II",
+    options: [
+      " Penemuan transistor",
+      "Pembuatan Internet",
+      "Peluncuran komputer Apple II",
+      "Terbitnya buku War Games",
+      "Pembentukan Perserikatan Bangsa-Bangsa"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Bagaimana peran komputer dalam Revolusi Industri 4.0 mempengaruhi lapangan pekerjaan dan sosial?",
+    answer: "Peningkatan ketergantungan pada teknologi",
+    options: [
+      "Peningkatan permintaan pekerja manual",
+      "Penurunan angka pengangguran",
+      "Peningkatan ketergantungan pada teknologi",
+      "Kehilangan minat dalam pendidikan tinggi",
+      "Meningkatnya pertumbuhan populasi dunia"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Komputer ENIAC, yang dibangun pada tahun 1940-an, memiliki dampak sosial besar karena:",
+    answer: "Memperluas kesenjangan gender di bidang teknologi",
+    options: [
+      "Mempercepat pertumbuhan ekonomi global",
+      "Mendorong penemuan roket antariksa",
+      "Memperluas kesenjangan gender di bidang teknologi",
+      "Membantu dalam perawatan kesehatan masyarakat",
+      "Menyebabkan kemunduran dalam perkembangan transportasi"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Perkembangan komputer pribadi seperti IBM PC pada tahun 1980-an memberikan akses kepada masyarakat untuk:",
+    answer: "Mengakses informasi dan komunikasi pribadi",
+    options: [
+      "Terhubung dengan antariksa",
+      "Menjalankan bisnis besar",
+      "Menjadi lebih terisolasi secara sosial",
+      "Mengakses informasi dan komunikasi pribadi",
+      "Mempertahankan gaya hidup tradisional"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?",
+    answer: "Hak untuk mengklaim kepemilikan atas karya kreatif",
+    options: [
+      "Hak untuk mengakses internet gratis",
+      "Hak untuk mengekspor perangkat lunak",
+      "Hak untuk mengklaim kepemilikan atas karya kreatif",
+      "Hak untuk menghapus data pribadi dari internet",
+      "Hak untuk menyalin dan mendistribusikan konten tanpa izin"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Bagaimana produk informatika berkontribusi terhadap pertumbuhan ekonomi?",
+    answer: "Dengan menciptakan lapangan kerja baru",
+    options: [
+      "Dengan membatasi akses informasi",
+      "Dengan meningkatkan biaya produksi",
+      "Dengan menciptakan lapangan kerja baru",
+      "Dengan mengurangi investasi dalam teknologi",
+      "Dengan meningkatkan kebutuhan akan bahan bakar fosil"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: " Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?",
+    answer: "Hak untuk mengklaim kepemilikan atas karya kreatif",
+    options: [
+      "Hak untuk mengakses internet gratis",
+      "Hak untuk mengekspor perangkat lunak",
+      "Hak untuk mengklaim kepemilikan atas karya kreatif",
+      "Hak untuk menghapus data pribadi dari internet",
+      "Hak untuk menyalin dan mendistribusikan konten tanpa izin"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: " Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?",
+    answer: "Hak untuk mengklaim kepemilikan atas karya kreatif",
+    options: [
+      "Hak untuk mengakses internet gratis",
+      "Hak untuk mengekspor perangkat lunak",
+      "Hak untuk mengklaim kepemilikan atas karya kreatif",
+      "Hak untuk menghapus data pribadi dari internet",
+      "Hak untuk menyalin dan mendistribusikan konten tanpa izin"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: " Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?",
+    answer: "Hak untuk mengklaim kepemilikan atas karya kreatif",
+    options: [
+      "Hak untuk mengakses internet gratis",
+      "Hak untuk mengekspor perangkat lunak",
+      "Hak untuk mengklaim kepemilikan atas karya kreatif",
+      "Hak untuk menghapus data pribadi dari internet",
+      "Hak untuk menyalin dan mendistribusikan konten tanpa izin"
+    ]
+  },
+
+
+  {
+    numb: 7,
+    question: "Bagaimana produk informatika berkontribusi terhadap pertumbuhan ekonomi?",
+    answer: "Dengan menciptakan lapangan kerja baru",
+    options: [
+      "Dengan membatasi akses informasi",
+      "Dengan meningkatkan biaya produksi",
+      "Dengan menciptakan lapangan kerja baru",
+      "Dengan mengurangi investasi dalam teknologi",
+      "Dengan meningkatkan kebutuhan akan bahan bakar fosil"
+    ]
+  },
+
+
+  {
+    numb: 8,
+    question: "Apa yang dimaksud dengan perangkat lunak sumber terbuka (open source software) dalam konteks ekonomi produk informatika?",
+    answer: "Perangkat lunak yang dapat dimodifikasi dan didistribusikan secara bebas",
+    options: [
+      "Perangkat lunak yang hanya bisa diakses oleh pembeli berlangganan",
+      "Perangkat lunak yang dapat dimodifikasi dan didistribusikan secara bebas",
+      "Perangkat lunak yang hanya tersedia untuk perusahaan besar",
+      "Perangkat lunak yang tidak dapat diakses secara online",
+      "Perangkat lunak yang hanya bisa diinstal pada satu perangkat saja"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: "Mengapa perlindungan data pribadi merupakan isu penting dalam hukum produk informatika?",
+    answer: "Untuk melindungi informasi pribadi pengguna",
+    options: [
+      "Untuk membatasi akses pengguna internet",
+      "Untuk mempromosikan pelanggaran privasi",
+      "Untuk melindungi informasi pribadi pengguna",
+      "Untuk meningkatkan risiko keamanan siber",
+      "Untuk membatasi inovasi teknologi"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Apa peran hukum dalam mengatur keamanan produk informatika?",
+    answer: "Menetapkan standar untuk perlindungan data dan privasi",
+    options: [
+      "Memperketat kontrol atas konten internet",
+      "Meningkatkan biaya produksi perangkat lunak",
+      "Menetapkan standar untuk perlindungan data dan privasi",
+      "Mengurangi inovasi dalam teknologi digital",
+      "Meniadakan akses internet di wilayah tertentu"
+    ]
+  },
+
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
